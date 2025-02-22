f1abb97464a2b0c08345930f5c9f20938a1d4b05
jrosa23 <jeremyrosario@MacBookPro.lan>
2025-02-21 21:04:16 -0500

    final commit

---
+++ static/js/logic.js
@@ -0,0 +1,106 @@
+// Create the 'basemap' tile layer that will be the background of our map.
+let basemap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap contributors"
+});
+
+// OPTIONAL: Step 2
+// Create the 'street' tile layer as a second background of the map
+let street = L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap contributors"
+});
+
+// Create the map object with center and zoom options.
+let map = L.map("map", {
+  center: [20, 0],
+  zoom: 2,
+  layers: [basemap] // Default layer
+});
+
+// Then add the 'basemap' tile layer to the map.
+basemap.addTo(map);
+// OPTIONAL: Step 2
+// Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
+// Add a control to the map that will allow the user to change which layers are visible.
+let earthquakes = new L.LayerGroup();
+let tectonicPlates = new L.LayerGroup();
+
+let baseMaps = {
+  "Basemap": basemap,
+  "Street": street
+};
+
+let overlays = {
+  "Earthquakes": earthquakes,
+  "Tectonic Plates": tectonicPlates
+};
+
+L.control.layers(baseMaps, overlays).addTo(map);
+// Make a request that retrieves the earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. Pass the magnitude and depth of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 0.8,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  function getColor(depth) {
+    return depth > 90 ? "#ea2c2c" :
+           depth > 70 ? "#ea822c" :
+           depth > 50 ? "#ee9c00" :
+           depth > 30 ? "#eecc00" :
+           depth > 10 ? "#d4ee00" : "#98ee00";
+  }
+
+  function getRadius(magnitude) {
+    return magnitude === 0 ? 1 : magnitude * 4;
+  }
+
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+  let legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+    let depths = [-10, 10, 30, 50, 70, 90];
+    let colors = ["#98ee00", "#d4ee00", "#eecc00", "#ee9c00", "#ea822c", "#ea2c2c"];
+    
+    for (let i = 0; i < depths.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+        depths[i] + (depths[i + 1] ? "&ndash;" + depths[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  legend.addTo(map);
+});
+
+// OPTIONAL: Step 2
+// Make a request to get our Tectonic Plate geoJSON data.
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plate_data) {
+  L.geoJson(plate_data, {
+    color: "orange",
+    weight: 2
+  }).addTo(tectonicPlates);
+
+  tectonicPlates.addTo(map);
+});
